d95775e31f4062f46e0cf40da4b68ccc01a9e0ae
Ali-Script <ali.prg01@gmail.com>
2023-12-16 19:34:54 +0330

    set new files

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$osname = $_POST['ali'];
-$myfile = fopen("ip.txt", "w");
-fwrite($myfile, $osname);
-fclose($myfile);
-$osname = $_POST['mms'];
-$myfile = fopen("data.json", "w");
-fwrite($myfile, $osname);
-fclose($myfile);
